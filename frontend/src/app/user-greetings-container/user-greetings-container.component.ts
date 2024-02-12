224c0618df4a5b457bba47943ef5fa9ce04d6656
Alan <alan-hd@hotmail.com>
2024-02-11 22:36:22 -0300

    Realizado a criação dos componentes

---
+++ frontend/src/app/user-greetings-container/user-greetings-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-greetings-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-greetings-container.component.html',
+  styleUrl: './user-greetings-container.component.css'
+})
+export class UserGreetingsContainerComponent {
+
+}
