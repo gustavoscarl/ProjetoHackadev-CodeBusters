ecf254341ff3fba3c72834849c844630bb333bc3
Alxd <alxd.paiva@outlook.com>
2024-02-16 20:25:33 -0300

    Versão com dark/light mode. Falta ajustes de imagens

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { HistoricoComponent } from './historico/historico.component';
 import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, LoginComponent, HomeComponent, HistoricoComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  texto:string = 'Hello, world!'
+}
