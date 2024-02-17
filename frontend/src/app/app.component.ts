a5f0e4573f95d40d0787814de61975c7e840e19a
Pedro Brandes <psbrandes@gmail.com>
2024-02-17 14:56:50 -0300

    Esqueleto inicial pagina histórico de transações

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HistoricoComponent } from './componentes/historico/historico.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HistoricoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
