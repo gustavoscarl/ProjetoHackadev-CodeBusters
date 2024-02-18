661cc2a6e7834a0858e20bc905fe7bc1fd98cacf
Alxd <alxd.paiva@outlook.com>
2024-02-18 16:28:27 -0300

    # Header finalizado com dark e light mode

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
-import { HistoricoComponent } from './historico/historico.component';
 import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent, HomeComponent, HistoricoComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
