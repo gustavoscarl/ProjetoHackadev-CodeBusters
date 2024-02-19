36acaff3272de0067e399f9bfff82c649e55126a
Pedro Brandes <psbrandes@gmail.com>
2024-02-19 14:57:14 -0300

    vetor transacoes

--- frontend/src/app/componentes/historico/historico.component.ts
+++ frontend/src/app/componentes/historico/historico.component.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter, Input, input } from '@angular/core';
 import { Transacao } from '../../modelo/Transacoes';
 import { CommonModule } from '@angular/common';
 import { TransacaoComponent } from '../transacao/transacao.component';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-historico',
@@ -10,17 +12,39 @@ import { TransacaoComponent } from '../transacao/transacao.component';
   templateUrl: './historico.component.html',
   styleUrl: './historico.component.css'
 })
+
+
 export class HistoricoComponent {
+
+  ngOnInit() {
+    this.ordenarTransacoes();
+  }
+  
 
     // Vetor de transacões - Onde as transações ficarão armazenadas no momento (sem backend)
   transacoes: Transacao[] = [
-    { tipo:'Pix', valor:120, contaDestino:202020, data:'2024-01-28'},
+    { tipo:'Pix', valor:120, contaDestino:123123, data:'2024-01-28'},
+    { tipo:'Debito', valor:15, contaDestino:111111, data:'2024-01-29'},
+    { tipo:'Credito', valor:1000, contaDestino:222222, data:'2024-01-30'},
     ];
 
   // FUNÇÃO QUE ADICIONA A TRANSAÇÃO NO VETOR DO HISTÓRICO.
   realizarTrasacao(obj:Transacao):void {
     this.transacoes.unshift(obj);
   }
+
+  // HISTÓRICO EXIBIDO, ORGANIZADO POR DIA.
+
+  historicotransacoes: Transacao[] = [];
+
+  ordenarTransacoes(){
+    this.transacoes.sort((a,b) => {
+      const dataA = new Date(a.data).getTime();
+      const dataB = new Date(b.data).getTime();
+      return dataB - dataA;
+    })
+  }
+
 
   transacaoFiltrada: Transacao[] = [];
 
