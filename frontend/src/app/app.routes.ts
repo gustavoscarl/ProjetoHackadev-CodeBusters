79581717d63f0093cdd52a1e7e755927aa4a39a7
Eduardo Araújo Dos Santos <104864916+E-A-D-S@users.noreply.github.com>
2024-02-22 00:27:55 -0300

    Inserindo tela de recuperar senha

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { ContainerComponentComponent } from './hompage/container-component/container-component.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { EsquecerSenhaComponent } from './esquecer-senha/esquecer-senha.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: ContainerComponentComponent},
   { path: 'cadastro', component: CadastroComponent},
+  {path: 'esquecer-senha', component: EsquecerSenhaComponent},
   // Redireciona para 'login' se a rota é o caminho raiz ou não é reconhecida
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
---
+++ frontend/src/app/esquecer-senha/esquecer-senha.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-esquecer-senha',
+  templateUrl: './esquecer-senha.component.html',
+  styleUrls: ['./esquecer-senha.component.css']
+})
+export class EsquecerSenhaComponent {
+
+}
