cd41007226b9e57dbaacd5b29378501e8ea2fbf7
Camila Vieira <camilag2.c@gmail.com>
2024-03-15 13:58:51 -0300

    Fixing Relationship

---
+++ PayWiseBackend/PayWiseBackend/Migrations/20240315165513_FixClienteDocumentoRelationship.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PayWiseBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixClienteDocumentoRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Clientes_Documentos_DocumentoId",
+                table: "Clientes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DocumentoId",
+                table: "Clientes",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Clientes_Documentos_DocumentoId",
+                table: "Clientes",
+                column: "DocumentoId",
+                principalTable: "Documentos",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Clientes_Documentos_DocumentoId",
+                table: "Clientes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DocumentoId",
+                table: "Clientes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Clientes_Documentos_DocumentoId",
+                table: "Clientes",
+                column: "DocumentoId",
+                principalTable: "Documentos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
