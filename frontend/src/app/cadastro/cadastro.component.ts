dd4e69c41923ea2dd0ab3b0ab277afd340d667bf
Pedro Brandes <psbrandes@gmail.com>
2024-03-15 19:30:28 -0300

    Adicionando api de endereço

--- frontend/src/app/cadastro/cadastro.component.ts
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -1,13 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { EnderecoService } from '../servicos/endereco.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, CommonModule, HttpClientModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
 
+
+  // Variavel CEP
+  cep:string = '';
+
+  constructor(private servico:EnderecoService){}
+
+  // Função para obter o endereço
+  obterEndereco():void{
+    this.servico.retornarEndereco(this.cep)
+    .subscribe( retorno => { console.log(retorno)});
+  }
 }
