695deb2fa6e729b013703c4c7b397be10859c9ba
Alan <alan-hd@hotmail.com>
2024-03-15 17:19:04 -0300

    Criada estrutura pin

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { EsquecerSenhaComponent } from './esquecer-senha/esquecer-senha.componen
 
 import { DepositoComponent } from './deposito/deposito.component';
 import { PixComponent } from './pix/pix.component';
+import { PinPadComponent } from './pin-pad/pin-pad.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -18,6 +19,7 @@ export const routes: Routes = [
   { path: 'transacao', component: TransacaoComponent},
   { path: 'pix', component: PixComponent},
   {path: 'deposito', component: DepositoComponent},
+  { path: 'pin-pad', component: PinPadComponent }, // Rota para a tela de digitação do PIN
   // Redireciona para 'login' se a rota é o caminho raiz ou não é reconhecida
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
