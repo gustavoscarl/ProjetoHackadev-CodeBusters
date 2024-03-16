26327dda4a46668b7a76b55003d3369d8db47035
Camila Vieira <camilag2.c@gmail.com>
2024-03-16 19:31:14 -0300

    Migration for Conta

--- PayWiseBackend/PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
+++ PayWiseBackend/PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d9fa322e889df669c7a68ca54538f29d95de52a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e26f44e8293bee2558ebc5c084d38dcfebaef68d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
