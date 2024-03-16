8d9fa322e889df669c7a68ca54538f29d95de52a
Camila Vieira <camilag2.c@gmail.com>
2024-03-16 19:08:56 -0300

    Adding Data Annotations

--- PayWiseBackend/PayWiseBackend/Domain/DTOs/CreateTransacaoDepositoDTO.cs
+++ PayWiseBackend/PayWiseBackend/Domain/DTOs/CreateTransacaoDepositoDTO.cs
@@ -1,7 +1,11 @@
-﻿namespace PayWiseBackend.Domain.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PayWiseBackend.Domain.DTOs;
 
 public class CreateTransacaoDepositoDTO
 {
+    [Required]
     public double Valor { get; set; }
+    [MaxLength(200)]
     public string? Descricao { get; set; }
 }
