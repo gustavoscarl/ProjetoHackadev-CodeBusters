b917478a94a83051c33964d5c3587bf8cfcc06c8
Camila Vieira <camilag2.c@gmail.com>
2024-03-16 19:54:19 -0300

    Migration for Endereco

--- PayWiseBackend/PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
+++ PayWiseBackend/PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e26f44e8293bee2558ebc5c084d38dcfebaef68d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26327dda4a46668b7a76b55003d3369d8db47035")]
 [assembly: System.Reflection.AssemblyProductAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
