278b2efd7b8b0ab94646ba101171be9e70c34deb
Pedro Brandes <psbrandes@gmail.com>
2024-03-16 12:38:05 -0300

    Adicionado endereço automatico via cep

--- frontend/src/app/cadastro/cadastro.component.ts
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { EnderecoService } from '../servicos/endereco.service';
 import { HttpClientModule } from '@angular/common/http';
+import { Endereco } from '../modelos/Endereco';
 
 @Component({
   selector: 'app-cadastro',
@@ -17,12 +18,33 @@ export class CadastroComponent {
 
   // Variavel CEP
   cep:string = '';
+  enderecoPorCep: Endereco | undefined;
 
   constructor(private servico:EnderecoService){}
 
-  // Função para obter o endereço
+  // Função para obter o endereço e chamar a função que preenche após obter os endereços via API.
+
+
   obterEndereco():void{
     this.servico.retornarEndereco(this.cep)
-    .subscribe( retorno => { console.log(retorno)});
+    .subscribe( retorno => { 
+      this.enderecoPorCep = retorno;
+      console.log(this.enderecoPorCep);
+      this.preencherCamposEndereco();
+    });
+  }
+
+
+  // Função que preenche os campos endereço
+  preencherCamposEndereco():void{
+
+    if(this.enderecoPorCep){
+      document.getElementById('inputCity')?.setAttribute('value', this.enderecoPorCep.localidade || '');
+      document.getElementById('inputLogradouro')?.setAttribute('value', this.enderecoPorCep.logradouro || '');
+      document.getElementById('inputBairro')?.setAttribute('value', this.enderecoPorCep.bairro || '');
+      document.getElementById('inputComplemento')?.setAttribute('value', this.enderecoPorCep.complemento || '');
+      (document.getElementById('inputState') as HTMLSelectElement).value = this.enderecoPorCep.uf || '';
+    } else { alert("preencha corretamente"); }
+    
   }
 }
