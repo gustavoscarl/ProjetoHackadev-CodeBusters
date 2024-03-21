b19cc0b7faa630fcb1cbfb07a9e18196560ece42
Dayane <dayanetm@outlook.com>
2024-03-21 20:14:03 -0300

    form validator pix

---
+++ frontend/src/app/modelos/Pix.ts
@@ -0,0 +1,6 @@
+export interface Pix {
+    // Atributos
+    valor?:number;
+    chavePix?:string;
+    descricao?:string;
+}
--- frontend/src/app/pix/pix.component.ts
+++ frontend/src/app/pix/pix.component.ts
@@ -1,23 +1,72 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
+import { PixService } from '../servicos/pix.service';
+import { Pix } from '../modelos/Pix'
 
 @Component({
   selector: 'app-pix',
   standalone: true,
-  imports: [FormsModule],
+  imports: [RouterModule, FormsModule, CommonModule, HttpClientModule, ReactiveFormsModule, NgxMaskDirective, NgxMaskPipe],
   templateUrl: './pix.component.html',
   styleUrl: './pix.component.css'
 })
+
 export class PixComponent {
   valor: number | undefined;
   chavePix: string | undefined;
   descricao: string | undefined;
+  pixService: any;
 
-  constructor() { }
+  constructor(private servico: PixService, private route:Router) { }
 
-  realizarTransacaoPix(): void {
+  efetuarPix(): void {
     // Lógica para realizar a transação PIX
     console.log('Transação PIX realizada:', { valor: this.valor, chavePix: this.chavePix, descricao: this.descricao });
     // Aqui você pode adicionar a lógica para enviar os detalhes da transação para o backend
+  }
+
+  // Form
+  pixForm!: FormGroup;
+  showAlert:boolean = false;
+  
+  ngOnInit() {
+    this.pixForm = new FormGroup({
+      'valor': new FormControl(null,
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]+$'),
+          Validators.minLength(1),
+        ]),
+      'chavePix': new FormControl(null,
+        [
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9.@_-]+$'),
+          Validators.minLength(6),
+        ]),
+        'descricao': new FormControl(null)
+    });
+  }
+  
+  onSubmit(): void {
+    console.log(this.pixForm);
+    this.pixForm.markAllAsTouched();
+
+    if(this.pixForm.valid) {
+      const pixData: Pix = {
+        valor: this.pixForm.get('valor')?.value,
+        chavePix: this.pixForm.get('chavePix')?.value,
+        descricao: this.pixForm.get('descricao')?.value,
+      }
+  
+    this.pixService.efetuarPix(pixData.valor, pixData.chavePix)
+      .efetuarPix((pix: Pix) => {
+        this.showAlert = true;
+        console.log(pix);
+      });
+    }
   }
 }
