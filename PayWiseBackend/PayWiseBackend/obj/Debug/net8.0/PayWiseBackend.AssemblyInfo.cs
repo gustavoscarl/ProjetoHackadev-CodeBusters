c18cdd430b3e01530b137bbfd756fa773aa7f1ca
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-03-22 13:57:49 -0300

    feature: adicionado página de modificação de conta

--- PayWiseBackend/PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
+++ PayWiseBackend/PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f00729013e38066d57bb92a180498e0e78c5d7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2ebd666d39a6eee88868eda7bd7b701eaaf7080")]
 [assembly: System.Reflection.AssemblyProductAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/app/conta/conta-criada/conta-criada.component.ts
+++ frontend/src/app/conta/conta-criada/conta-criada.component.ts
@@ -1,13 +1,65 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { CriarContaService } from '../../servicos/criarconta.service';
+import { AuthService } from '../../auth.service';
+import { MudarConta } from '../../modelos/MudarConta';
+import { MudarContaService } from '../../servicos/mudar-conta.service';
+import { ContaInfoService } from '../../servicos/getcontainfo.service';
+import { NgxCurrencyDirective, NgxCurrencyInputMode } from 'ngx-currency';
 
 @Component({
   selector: 'app-conta-criada',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule, NgxCurrencyDirective],
   templateUrl: './conta-criada.component.html',
   styleUrl: './conta-criada.component.css'
 })
 export class ContaCriadaComponent {
+  changeAccountForm!: FormGroup
 
+  ngOnInit() {
+    this.changeAccountForm = new FormGroup({
+      'pix-geral': new FormControl(null, 
+        [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+      'pix-noturno': new FormControl(null, 
+        [
+        Validators.required 
+      ])})
+
+      this.contaInfoService.getInformacoes().subscribe({
+        next: (retorno: any) => {
+
+        }
+      })
+
+    }
+
+  constructor(private contaService:MudarContaService, private authService: AuthService, private route: Router, private contaInfoService: ContaInfoService) {}
+
+  onSubmit(): void {
+    console.log(this.changeAccountForm)
+    this.changeAccountForm?.markAllAsTouched();
+    if (this.changeAccountForm?.valid) {
+      let mudarContaData: MudarConta = {
+        limitePixGeral: this.changeAccountForm.get('pix-geral')?.value,
+        limitePixNoturno: this.changeAccountForm.get('pix-noturno')?.value
+      }
+      this.contaService.cadastrarConta(mudarContaData)
+        .subscribe({
+          next: (retorno: any) => {
+            this.changeAccountForm.reset();
+            setTimeout(() => {
+              this.route.navigate(['/home']);
+            }, 1200);
+          },
+          error: (error) => {
+            console.log(error);
+          }
+        });
+    }
+  }
 }
