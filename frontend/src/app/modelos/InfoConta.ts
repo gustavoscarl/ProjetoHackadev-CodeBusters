0a59e0826f5134550661cb6f121a9c8050f87cc5
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-03-22 19:22:37 -0300

    mudanças em contas

---
+++ frontend/src/app/modelos/InfoConta.ts
@@ -0,0 +1,9 @@
+export interface InfoConta {
+    id?: number,
+    numero?: string,
+    saldo?: number,
+    agencia?: string,
+    limitePixGeral?: number,
+    limitePixNoturno?: number,
+    historicoUrl?: string
+}
