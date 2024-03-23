cd896a0669f5a0482e5843c725700acb5644848b
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-03-23 11:03:30 -0300

    refresh and access token in interceptor and backend

--- PayWiseBackend/PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
+++ PayWiseBackend/PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a52c66c7c9fc0cd1ed87e929fb2f121fc37d3dde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a5d324fc323863af35eb8f058ed3d966e23c3e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
