675569d1e718bb9ec6b1c7d926b0fe22aefc35e1
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-03-24 08:49:55 -0300

    context result

---
+++ PayWiseBackend/PayWiseBackend/Domain/Context/ContextResults/ContextResult.cs
@@ -0,0 +1,12 @@
+﻿namespace PayWiseBackend.Domain.Context.ContextResults;
+
+public class ContextResult<T> : IContextResult<T>
+{
+    public T Data { get; private set; }
+    public bool Sucesso { get; private set; }
+    public string MensagemDeErro { get; private set; }
+
+    public static ContextResult<T> ResultadoSucesso(T data) => new ContextResult<T> { Sucesso = true, Data = data };
+    public static ContextResult<T> ResultadoFalha(string mensagemDeErro) => new ContextResult<T> { Sucesso = false, MensagemDeErro = mensagemDeErro };
+
+}
